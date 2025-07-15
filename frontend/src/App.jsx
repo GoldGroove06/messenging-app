8466aacfc6ead40235e3f96c21f774b9c0998e6e
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-07-16 00:00:10 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <ul id="messages"></ul>
+    <form id="form" action="">
+      <input id="input" autocomplete="off" /><button>Send</button>
+    </form>
+      </div>
+    </>
+  )
+}
+
+export default App
