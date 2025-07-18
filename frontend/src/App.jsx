9c137f3af59f5cdfa12da5054efac38151253ccb
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-07-19 01:44:08 +0530

    cors chat work in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,44 @@
-
+import React,{ useState, useEffect } from 'react'
 
 import './App.css'
+import {io} from 'socket.io-client';
+
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const socketInstance = io('http://localhost:3000');
+    setSocket(socketInstance);
+
+    socketInstance.on('chat message', (msg) => {
+      setMessages((prevMessages) => [...prevMessages, msg]);
+
+    });
+  },[])
+ 
+
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    socket.emit('chat message', input)
+    setInput('')
+  }
+
 
 
   return (
     <>
       <div>
-        <ul id="messages"></ul>
-    <form id="form" action="">
-      <input id="input" autocomplete="off" /><button>Send</button>
-    </form>
+        <ul id="messages">{messages.map((message) => <li>{message}</li>)}</ul>
+    <div id="form">
+      <input id="input" autocomplete="off" value={input} onChange={(e) => setInput(e.target.value)}/><button onClick={handleSubmit}>Send</button>
+ </div>
       </div>
     </>
   )
